7369c99782edbdf17e711b74a571c38f9f431e3d
Whitw-pers <120962306+Whitw-pers@users.noreply.github.com>
2024-06-17 12:16:23 -0600

    added basic motion

---
+++ lesson_3_basic_motion/lesson_3_basic_motion.ino
@@ -0,0 +1,22 @@
+/* 
+Whit Whittall
+COSGC New Robotics Workshop template code for lesson 3 basic motion
+*/ 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//--------------------CUSTOM FXNS--------------------
+// void motor_controller(v, w)
+
+void motor_controller(float v, float w) {
+// 
+
+}
