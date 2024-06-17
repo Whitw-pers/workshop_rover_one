4d9c701d376bf5d1fa65690a9218d7346b1207d4
Whitw-pers <120962306+Whitw-pers@users.noreply.github.com>
2024-06-17 16:00:58 -0600

    wrote motor controller

--- lesson_3_basic_motion/lesson_3_basic_motion.ino
+++ lesson_3_basic_motion/lesson_3_basic_motion.ino
@@ -1,6 +1,6 @@
 /* 
-Whit Whittall
-COSGC New Robotics Workshop template code for lesson 3 basic motion
+By Whit Whittall
+COSGC New Robotics Workshop code for lesson 3 basic motion
 
 robot is oriented correctly when the USB in of the arduino is_____
 left and right directions referenced in comments are from the robot's perspective
@@ -21,12 +21,6 @@ const int L1 = 7;    //BI1
 const int L2 = 8;    //BI2
 const int pwmL = 9;  //PWMB
 
-//--------------------initialize velocity variables--------------------
-float v = 0;      // linear velocity (positive is drive forward, negative is drive backwards)
-float w = 0;      // angular velocity (positive is rotate left, negative is rotate right)
-float dphi_R = 0;  // right wheel velocity
-float dphi_L = 0;  // left wheel velocity
-
 void setup() {
   // put your setup code here, to run once:
 
@@ -42,16 +36,65 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // declaring v and w here limits their scope to the main loop so there aren't conflicts with motor_controller()
+
+  //--------------------declare velocity variables--------------------
+  int v = 0;        // linear velocity (100 forward full, -100 is back full)
+  int w = 0;        // angular velocity (100 is rotate left full, -100 is rotate right full)
 
 }
 
 //--------------------CUSTOM FXNS--------------------
 // void motor_controller(v, w)
+// void drive(vel_L, vel_R)
 
-void motor_controller(float v, float w) {
+void motor_controller(int v, int w) {
 // gets the desired linear and angular velocity of our robot
 // expects -1 < v, w < 1
 // determines required wheel speeds based on linear and angular velocities
 // maps required wheel speeds to PWM duty cycle
 
+  int dphi_L = (v/r) - (L * w)/(2 * r);
+  int dphi_R = (v/r) + (L * w)/(2 * r);
+
+  dphi_L = constrain(dphi_L, -100, 100);
+  dphi_R = constrain(dphi_R, -100, 100);
+
+  dphi_L = map(dphi_L, -100, 100, -255, 255);
+  dphi_R = map(dphi_R, -100, 100, -255, 255);
+
+  drive(dphi_L, dphi_R);
+}
+
+void drive(int vel_L, int vel_R) {
+// expects vel_L and vel_R to be between -255 and 255
+
+  // left motor
+  if (spd_L > 0) {  // left motor forward
+    digitalWrite(L1, HIGH);
+    digitalWrite(L2, LOW);
+  }
+  if (spd_L < 0) {  // left motor backward
+    digitalWrite(L1, LOW);
+    digitalWrite(L2, HIGH);
+  }
+  if (spd_L == 0) {  // left motor forward
+    digitalWrite(L1, LOW);
+    digitalWrite(L2, LOW);
+  }
+  // right motor
+if (spd_R > 0) {  // right motor forward
+    digitalWrite(R1, HIGH);
+    digitalWrite(R2, LOW);
+  }
+  if (spd_R < 0) {  // right motor backward
+    digitalWrite(R1, LOW);
+    digitalWrite(R2, HIGH);
+  }
+  if (spd_R == 0) {  // right motor forward
+    digitalWrite(R1, LOW);
+    digitalWrite(R2, LOW);
+  }
+  analogWrite(pwmL, abs(spd_L));
+  analogWrite(pwmR, abs(spd_R));
 }
